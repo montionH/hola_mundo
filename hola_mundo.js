fdfa92135aa41603e7e56c56857f9e2a1e7ca764
montionH <66965471+montionH@users.noreply.github.com>
2021-11-03 11:35:31 -0700

    Add files via upload
    archivo de hola mundo que imprime mensaje cada segundo

---
+++ hola_mundo.js
@@ -0,0 +1,8 @@
+'use strict'
+console.log('Hola mundo Node.js esto se vera en la terminal de comandos')
+console.log(2 * 5)
+    //console.log(window)
+console.log(global)
+setInterval(function() {
+    console.log('hola node.js')
+}, 1000)
